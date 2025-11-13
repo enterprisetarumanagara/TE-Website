aa887245db837e5e22c61951b214a13f00cade60
hanseehhh <mrhns00@gmail.com>
2025-11-13 11:21:50 +0700

    add: programs section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/sections/Hero";
+import WhatWeDo from "@/components/sections/WhatWeDo";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
+      <WhatWeDo />
     </main>
   );
 }
