f7c9c32c675956a1302990d369f34e484cc1c4c7
hanseehhh <mrhns00@gmail.com>
2025-11-13 13:15:22 +0700

    feat: add tpc section about upcoming events

--- src/app/tpc/page.tsx
+++ src/app/tpc/page.tsx
@@ -1,11 +1,12 @@
 import TpcHero from "@/app/tpc/components/tpcHero";
 import TpcWhatWeDo from "@/app/tpc/components/tpcWhatWeDo";
-
+import TpcUpcoming from "@/app/tpc/components/tpcUpcoming";
 export default function TpcPage() {
   return (
     <main>
       <TpcHero />
       <TpcWhatWeDo />
+      <TpcUpcoming />
     </main>
   );
 }
